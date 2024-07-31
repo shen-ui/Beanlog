6dec771509947aefcfa393557c61dc2aa3f9107e
Shen Wang <peterhuiwang1995@gmail.com>
2024-07-31 02:30:52 -0400

    init

---
+++ src/main/java/com/example/beanlog/BeanLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanLogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/beanlog/beans/Beans.java
@@ -0,0 +1,11 @@
+package com.example.beanlog.beans;
+
+public class Beans {
+    Integer id;
+    String name;
+    String roast_level;
+    String origin;
+    String[] tasting_notes;
+    Double price_per_lb;
+
+}
